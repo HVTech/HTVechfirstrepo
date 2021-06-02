a992d95099d1008efe4a88b078672b5b7ba80a94
NogTai <thetai2382002@gmail.com>
2021-06-02 22:14:58 +0700

    Hoan thanh 5 chuc nang

--- Binhga.cpp
+++ Binhga.cpp
@@ -41,6 +41,7 @@ void Input_data(contact *data_user)
         cout << "----------------------------------------\n";
     }
 }
+//-------------------------------
 
 void Tim_kiem_theo_sdt(contact *data_user)
 {
@@ -69,6 +70,7 @@ void Tim_kiem_theo_sdt(contact *data_user)
     if (!check)
         cout << "Nguoi ban tim khong co trong du lieu1!";
 }
+//---------------------------
 
 void Tim_kiem_theo_email(contact *data_user)
 {
@@ -97,6 +99,7 @@ void Tim_kiem_theo_email(contact *data_user)
     if (!check)
         cout << "Nguoi ban tim khong co trong du lieu2!";
 }
+//---------------------------
 
 void Search_data(contact *data_user)
 {
@@ -112,18 +115,7 @@ void Search_data(contact *data_user)
     else if(chosse == 2)
         Tim_kiem_theo_email(data_user);
 }
-
-contact *Tao_du_lieu_moi(contact *data_user)
-{
-    delete data_user;
-    contact *data_user2 = new contact[10000];
-    return data_user2;
-}
-
-void Xoa_du_lieu_cu(contact *data_user)
-{
-    delete data_user;
-}
+//----------------------------
 
 void Xuat_danh_ba(contact *data_user)
 {
@@ -142,47 +134,107 @@ void Xuat_danh_ba(contact *data_user)
         cout << "\n----------------------------------------\n";
     }
 }
+//-----------------------------
+
+void Remove_user(contact *data_user)
+{
+    int index;
+    cout << "Nhap so thu tu cua danh ba muon xoa: ";
+    cin >> index;
+    index --;
+    for (int i=index; i<n-1; i++)
+    {
+        data_user[i] = data_user[i+1];
+    }
+    n --;
+}
+
+void Add_user(contact *data_user)
+{
+    cin.ignore();
+    cout << "Nhap thong tin cua nguoi dung thu " << n+1;
+    cout << "\nHo va ten: "; getline(cin, data_user[n].Name);
+    cout << "ID: "; getline(cin, data_user[n].id);
+    cout << "Ngay sinh (dd/mm/yy): "; getline(cin, data_user[n].DOB);
+    cout << "So dien thoai: "; getline(cin, data_user[n].phone);
+    cout << "Email: "; getline(cin, data_user[n].email);
+    cout << "Dia chi: "; getline(cin, data_user[n].Address);
+    cout << "Nguoi phu trach: "; getline(cin, data_user[n].assignee);
+    cout << "----------------------------------------\n";
+    n ++;
+}
+
+void Edit_user(contact *data_user)
+{
+    int num;
+    cout << "Nhap so danh ba can sua: ";
+    cin >> num;
+    //Sua thong tin
+    cin.ignore();
+    cout << "Nhap thong tin cua nguoi dung thu " << num;
+    cout << "\nHo va ten: "; getline(cin, data_user[num-1].Name);
+    cout << "ID: "; getline(cin, data_user[num-1].id);
+    cout << "Ngay sinh (dd/mm/yy): "; getline(cin, data_user[num-1].DOB);
+    cout << "So dien thoai: "; getline(cin, data_user[num-1].phone);
+    cout << "Email: "; getline(cin, data_user[num-1].email);
+    cout << "Dia chi: "; getline(cin, data_user[num-1].Address);
+    cout << "Nguoi phu trach: "; getline(cin, data_user[num-1].assignee);
+    cout << "----------------------------------------\n";
+}
 
 int main()
 {
-    contact *data_user = new contact[10000];
-    //Input_data(data_user);
+    contact *data_user = new contact[100];
+    cout << "Can tao danh ba moi khi dung lan dau dung chuong trinh!\n";
+    Input_data(data_user);
+
+    //Hien thi cac chuc nang
     cout << "1. Tao danh ba moi";
     cout << "\n2. Chinh sua danh ba";
     cout << "\n3. Xoa danh ba";
-    cout << "\n4. Tim danh ba theo ten";
-    cout << "\n5. Tim danh ba theo SDT";
-    cout << "\n6. Tim danh ba theo khu vuc";
-    cout << "\n7. Tim danh ba thieu email";
-    cout << "\n8. Tim nhung SDT duoc giao cho 1 nhan vien";
+    cout << "\n4. Xuat danh ba";
+    cout << "\n5. Tim danh ba";
+    cout << "\n6. Tim danh ba thieu email";
+    cout << "\n7. Tim nhung SDT duoc giao cho 1 nhan vien";
     cout << "\n0. Thoat chuong trinh";
     cout << "\nNhap lua chon cua ban: ";
-    /*int lua_chon;
+    int lua_chon;
+    bool thoat = false;
+    cin >> lua_chon;
     do
     {
-        cin >> lua_chon;
         switch (lua_chon)
         {
         case 1:
-            Input_data(data_user);
+            Add_user(data_user);
+            cout << "\nNhap lua chon cua ban: ";
+            cin >> lua_chon;
+            break;
+        case 2:
+            Edit_user(data_user);
+            cout << "\nNhap lua chon cua ban: ";
+            cin >> lua_chon;
             break;
         case 3:
-            Xoa_du_lieu_cu(data_user);
+            Remove_user(data_user);
+            cout << "\nNhap lua chon cua ban: ";
+            cin >> lua_chon;
             break;
         case 4:
-            Search_data(data_user);
+            Xuat_danh_ba(data_user);
+            cout << "\nNhap lua chon cua ban: ";
+            cin >> lua_chon;
             break;
         case 5:
             Search_data(data_user);
+            cout << "\nNhap lua chon cua ban: ";
+            cin >> lua_chon;
+        case 0:
+            break;
         default:
-            cout << "Khong co lua chon phu hop";
+            lua_chon = 0;
             break;
         }
-    }
-    while (lua_chon != 0);  */
-    Input_data(data_user);
-    Search_data(data_user);
-    data_user = Tao_du_lieu_moi(data_user);
-    Xuat_danh_ba(data_user);
+    } while (lua_chon != 0);
     delete data_user;
 }
